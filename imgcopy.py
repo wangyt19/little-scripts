3fbf88d59279bfcebec5b4e421ba83592f7e2c1d
wangyt19 <35524637+wangyt19@users.noreply.github.com>
2018-10-23 11:31:43 +0800

    Update imgcopy.py

--- imgcopy.py
+++ imgcopy.py
@@ -6,5 +6,6 @@ def Copyfile(fileDir):
   sample=random.sample(pathDir,1000)
   for name in sample:
     shutil.copyfile(fileDir+name,tarDir+name)
+    #shutil.move(fileDir+name,tarDir+name)
 if __name__=='__main__':
   Copyfile(fileDir)
