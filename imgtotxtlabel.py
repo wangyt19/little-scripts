7862f2e1000a8985c6182899bb92d4d34b9fcdf0
wangyt19 <35524637+wangyt19@users.noreply.github.com>
2018-10-23 16:24:55 +0800

    Update imgtotxtlabel.py

--- imgtotxtlabel.py
+++ imgtotxtlabel.py
@@ -4,7 +4,7 @@ dir_label=dir_img+'_label'
 def makedirs_quietly(d):
   if not os.path.exists(d):
     os.makedirs(d)
-makedirs_quietly(d)
+makedirs_quietly(dir_label)
 for x in os.listdir(dir_image):
   txt_path=os.path.join(dir_label,x.split('.')[0]+'.txt')
   with open(txt_path,'w') as f:
