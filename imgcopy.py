a9ba4b0a6f679969488bb49add56d75c72f234d1
wangyt19 <35524637+wangyt19@users.noreply.github.com>
2018-10-23 11:29:20 +0800

    Create imgcopy.py

---
+++ imgcopy.py
@@ -0,0 +1,10 @@
+fileDir='A'
+tarDir='B'
+import os,shutil,random
+def Copyfile(fileDir):
+  pathDir=os.listdir(fileDir)
+  sample=random.sample(pathDir,1000)
+  for name in sample:
+    shutil.copyfile(fileDir+name,tarDir+name)
+if __name__=='__main__':
+  Copyfile(fileDir)
