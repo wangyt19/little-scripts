dbedd13c941549b58ab8e7704fcaa9929fc07f5a
wangyt19 <35524637+wangyt19@users.noreply.github.com>
2018-11-14 17:27:44 +0800

    Update pandasmergecsv.py

--- pandasmergecsv.py
+++ pandasmergecsv.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as np
 df1=pd.read_csv('./ccc/a.csv',dtype={'prediction':np.object})
-df1.rename(colums={'image':'image','prediction':'收款人账号'},inplace=True)#true代表在原来的基础上修改
+df1.rename(columns={'image':'image','prediction':'收款人账号'},inplace=True)#true代表在原来的基础上修改
 df2=pd.read_csv('./ccc/b.csv',dtype={'prediction':np.object})
-df2.rename(colums={'image':'image','prediction':'收款人名称'},inplace=True)
+df2.rename(columns={'image':'image','prediction':'收款人名称'},inplace=True)
 df3=pd.read_csv('./ccc/c.csv',dtype={'prediction':np.float})
 df3['prediction']=df3['prediction'].map(lambda x: x/100)
-df3.rename(colums={'image':'image','prediction':'小写金额'},inplace=True)
+df3.rename(columns={'image':'image','prediction':'小写金额'},inplace=True)
 
 df=pd.merge(df1,df2,df3,on=[image])
 
