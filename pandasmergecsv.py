bc145b3c165265c837a38e40d190a3ebe52e2d0c
wangyt19 <35524637+wangyt19@users.noreply.github.com>
2018-11-14 10:42:23 +0800

    Update pandasmergecsv.py

--- pandasmergecsv.py
+++ pandasmergecsv.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import numpy as np
-df1=pd.read_csv('./ccc/a.csv')
+df1=pd.read_csv('./ccc/a.csv',dtype={'prediction':np.object})
 df1.rename(colums={'image':'image','prediction':'收款人账号'},inplace=True)#true代表在原来的基础上修改
-df2=pd.read_csv('./ccc/b.csv')
+df2=pd.read_csv('./ccc/b.csv',dtype={'prediction':np.object})
 df2.rename(colums={'image':'image','prediction':'收款人名称'},inplace=True)
-df=pd.merge(df1,df2,on=[image])
+df3=pd.read_csv('./ccc/c.csv',dtype={'prediction':np.float})
+df3['prediction']=df3['prediction'].map(lambda x: x/100)
+df3.rename(colums={'image':'image','prediction':'小写金额'},inplace=True)
+
+df=pd.merge(df1,df2,df3,on=[image])
 
 df.to_csv('./ccc/test.csv',header=True,index=False,float_format='%.2f')
